a8ba6d4d59c46109954c8ad628f2b71dc4e6fec3
tor2402 <hectorrover5@gmail.com>
2025-06-05 22:13:58 -0500

    version2

---
+++ bucles.py
@@ -0,0 +1,10 @@
+
+
+datos = ["12", "abc", "45", None, "99"]
+for dato in datos:
+    try:
+        numero = int(dato)
+        print(f"Convertido: {numero}")
+    except (ValueError, TypeError):
+        print(f"Error al convertir: {dato}")
+
---
+++ condicionales.py
@@ -0,0 +1,10 @@
+valores = [4, 6, 8, 10]
+
+if all(x % 2 == 0 for x in valores):
+    print("Todos son pares")
+elif any(x % 2 == 0 for x in valores):
+    print("Algunos son pares")
+else:
+    print("Ninguno es par")
+
+
